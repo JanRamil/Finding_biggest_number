a82d947f5c37109e48a0c033cc4cc0f719b64857
JAN RAMIL ELIZER ALARCON <janramila@gmail.com>
2024-01-08 12:53:01 +0800

    Adjusted the placement of the frames and revised the code for the result

--- finding_biggest_number.py
+++ finding_biggest_number.py
@@ -33,8 +33,9 @@ def start_clicked():
                 else:
                     result = f"The largest number is: {input_3}"
 
-                # Display the result using a messagebox
-                messagebox.showinfo("Result", result)
+                # Display the result
+                result_label = tk.Label(window, text=result, font=("Cooper Black", 12), bg="#F8EFE4", fg="#ffcd58")
+                result_label.place(relx=0.71, rely=0.75, anchor="center")
 
             except ValueError:
                 messagebox.showerror("Error", "Please enter valid numbers.")
@@ -55,28 +56,28 @@ def start_clicked():
     background_label.place(relwidth=1, relheight=1)
     
     # Create entry widgets for user input
-    entry_frame = tk.Frame(window, pady=10)
-    entry_frame.pack()
-
+    entry_frame = tk.Frame(window, pady=10, bg="#F8EFE4")
+    entry_frame.place(relx=0.6, rely=0.55)
+    
     label1 = tk.Label(entry_frame, text="Enter number 1:", font=("Cooper Black", 10), bg="#F8EFE4", fg="#FF005C")
-    label1.place(relx=0.3, rely=0.2)
+    label1.grid(row=0, column=0, padx=5)
     entry1 = tk.Entry(entry_frame)
-    entry1.place(relx=0.3, rely=0.2)
+    entry1.grid(row=0, column=1)
 
     label2 = tk.Label(entry_frame, text="Enter number 2:", font=("Cooper Black", 10), bg="#F8EFE4", fg="#FF005C")
-    label2.place(relx=0.7, rely=0.6)
+    label2.grid(row=1, column=0, padx=5)
     entry2 = tk.Entry(entry_frame)
-    entry2.place(relx=0.7, rely=0.6)
+    entry2.grid(row=1, column=1)
 
     label3 = tk.Label(entry_frame, text="Enter number 3:", font=("Cooper Black", 10), bg="#F8EFE4", fg="#FF005C")
-    label3.place(relx=0.7, rely=0.6)
+    label3.grid(row=2, column=0, padx=5)
     entry3 = tk.Entry(entry_frame)
-    entry3.place(relx=0.7, rely=0.6)
+    entry3.grid(row=2, column=1)
     
 
     # Create buttons
     calculate_button = tk.Button(window, text="Find Largest Number", command=find_largest_number, pady=10, font=("Cooper Black", 10), bg="#F8EFE4", fg="#3E630F")
-    calculate_button.pack()
+    calculate_button.place(relx=0.65, rely=0.65)
 
 
     # Start the Tkinter event loop
